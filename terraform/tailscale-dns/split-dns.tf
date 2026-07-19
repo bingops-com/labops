23912a43bd5790bbe12cbc23c389a2157b7072ef
0xBingo <bingops@pm.me>
2026-07-19 17:34:51 +0200

    feat(gitops): move lab endpoints to lab.bingo

--- terraform/tailscale-dns/split-dns.tf
+++ terraform/tailscale-dns/split-dns.tf
@@ -1,9 +1,9 @@
 resource "tailscale_dns_split_nameservers" "labtest" {
-  domain      = "test.bingops.com"
+  domain      = "test.lab.bingo"
   nameservers = ["192.168.1.152"]
 }
 
 resource "tailscale_dns_split_nameservers" "argocd_labprod" {
-  domain      = "argocd.bingops.com"
+  domain      = "argocd.lab.bingo"
   nameservers = ["192.168.1.151"]
 }
